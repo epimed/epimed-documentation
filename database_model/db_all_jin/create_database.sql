a63a8047e817415e2c61dff89c8a117763f34449
Ekaterina Flin <ekaterina.flin@univ-grenoble-alpes.fr>
2016-10-12 15:27:55 +0200

    Add schemas

--- database_model/db_all_jin/create_database.sql
+++ database_model/db_all_jin/create_database.sql
@@ -1,46 +1,63 @@
 /*==============================================================*/
 /* Nom de SGBD :  PostgreSQL 8                                  */
-/* Date de création :  04/10/2016 14:51:51                      */
+/* Date de création :  10/10/2016 15:18:28                      */
 /*==============================================================*/
 
 
-drop table ALV_CODE;
+drop table ALL_ALV_CODE;
 
-drop table GENE;
+drop table ALL_GENE;
 
-drop table PATIENT;
+drop table ALL_NORM;
 
-drop table PRIMER;
+drop table ALL_PATIENT;
 
-drop table QPCR;
+drop table ALL_PRIMER;
 
-drop table SAMPLE;
+drop table ALL_QPCR;
+
+drop table ALL_SAMPLE;
 
 /*==============================================================*/
-/* Table : ALV_CODE                                             */
+/* Table : ALL_ALV_CODE                                         */
 /*==============================================================*/
-create table ALV_CODE (
+create table ALL_ALV_CODE (
    ID_ALV               VARCHAR(50)          not null,
    ID_PATIENT           VARCHAR(50)          not null,
-   constraint PK_ALV_CODE primary key (ID_ALV)
+   constraint PK_ALL_ALV_CODE primary key (ID_ALV)
 );
 
 /*==============================================================*/
-/* Table : GENE                                                 */
+/* Table : ALL_GENE                                             */
 /*==============================================================*/
-create table GENE (
+create table ALL_GENE (
    ID_GENE              VARCHAR(50)          not null,
    INPUT_NAME           VARCHAR(50)          not null,
    ENTREZ               INT4                 null,
    GENE_SYMBOL          VARCHAR(50)          null,
    TITLE                VARCHAR(255)         null,
-   constraint PK_GENE primary key (ID_GENE)
+   CTL                  boolean              not null,
+   constraint PK_ALL_GENE primary key (ID_GENE)
 );
 
 /*==============================================================*/
-/* Table : PATIENT                                              */
+/* Table : ALL_NORM                                             */
 /*==============================================================*/
-create table PATIENT (
+create table ALL_NORM (
+   ID_NORM              INT4                 not null,
+   ID_PRIMER            VARCHAR(50)          null,
+   ID_SAMPLE            VARCHAR(50)          null,
+   TISSUES_CTL          VARCHAR(255)         null,
+   GENES_CTL            VARCHAR(255)         null,
+   DELTA_DELTA_CT       FLOAT8               null,
+   EXPRESSION_LEVEL     FLOAT8               null,
+   constraint PK_ALL_NORM primary key (ID_NORM)
+);
+
+/*==============================================================*/
+/* Table : ALL_PATIENT                                          */
+/*==============================================================*/
+create table ALL_PATIENT (
    ID_PATIENT           VARCHAR(50)          not null,
    CODE                 VARCHAR(50)          not null,
    NAME                 VARCHAR(100)         not null,
@@ -67,65 +84,77 @@ create table PATIENT (
    CELL_TUBE            VARCHAR(255)         null,
    G6_ALLPOS_1          VARCHAR(50)          not null,
    G6_ALLPOS_2          VARCHAR(50)          not null,
-   constraint PK_PATIENT primary key (ID_PATIENT)
+   constraint PK_ALL_PATIENT primary key (ID_PATIENT)
 );
 
 /*==============================================================*/
-/* Table : PRIMER                                               */
+/* Table : ALL_PRIMER                                           */
 /*==============================================================*/
-create table PRIMER (
+create table ALL_PRIMER (
    ID_PRIMER            VARCHAR(50)          not null,
-   ID_GENE              VARCHAR(50)          null,
+   ID_GENE              VARCHAR(50)          not null,
    NUMBER               INT4                 not null,
    FORWARD              TEXT                 null,
    REVERSE              TEXT                 null,
-   constraint PK_PRIMER primary key (ID_PRIMER)
+   constraint PK_ALL_PRIMER primary key (ID_PRIMER)
 );
 
 /*==============================================================*/
-/* Table : QPCR                                                 */
+/* Table : ALL_QPCR                                             */
 /*==============================================================*/
-create table QPCR (
+create table ALL_QPCR (
    ID_QPCR              INT4                 not null,
    ID_PRIMER            VARCHAR(50)          null,
    ID_SAMPLE            VARCHAR(50)          null,
    CT                   FLOAT8               null,
-   RED_CONTROL          boolean              null,
-   constraint PK_QPCR primary key (ID_QPCR)
+   RED                  boolean              null,
+   constraint PK_ALL_QPCR primary key (ID_QPCR)
 );
 
 /*==============================================================*/
-/* Table : SAMPLE                                               */
+/* Table : ALL_SAMPLE                                           */
 /*==============================================================*/
-create table SAMPLE (
+create table ALL_SAMPLE (
    ID_SAMPLE            VARCHAR(50)          not null,
    ID_PATIENT           VARCHAR(50)          null,
    COMMENT              VARCHAR(100)         null,
-   constraint PK_SAMPLE primary key (ID_SAMPLE)
+   ENABLED              boolean              not null,
+   TISSUE               VARCHAR(255)         null,
+   constraint PK_ALL_SAMPLE primary key (ID_SAMPLE)
 );
 
-alter table ALV_CODE
+alter table ALL_ALV_CODE
    add constraint fk_alv_code_patient foreign key (ID_PATIENT)
-      references PATIENT (ID_PATIENT)
+      references ALL_PATIENT (ID_PATIENT)
       on delete restrict on update restrict;
 
-alter table PRIMER
+alter table ALL_NORM
+   add constraint fk_norm_primer foreign key (ID_PRIMER)
+      references ALL_PRIMER (ID_PRIMER)
+      on delete restrict on update restrict;
+
+alter table ALL_NORM
+   add constraint fk_norm_sample foreign key (ID_SAMPLE)
+      references ALL_SAMPLE (ID_SAMPLE)
+      on delete restrict on update restrict;
+
+alter table ALL_PRIMER
    add constraint fk_primer_gene foreign key (ID_GENE)
-      references GENE (ID_GENE)
+      references ALL_GENE (ID_GENE)
       on delete restrict on update restrict;
 
-alter table QPCR
+alter table ALL_QPCR
    add constraint fk_qpcr_primer foreign key (ID_PRIMER)
-      references PRIMER (ID_PRIMER)
+      references ALL_PRIMER (ID_PRIMER)
       on delete restrict on update restrict;
 
-alter table QPCR
+alter table ALL_QPCR
    add constraint fk_qpcr_sample foreign key (ID_SAMPLE)
-      references SAMPLE (ID_SAMPLE)
+      references ALL_SAMPLE (ID_SAMPLE)
       on delete restrict on update restrict;
 
-alter table SAMPLE
+alter table ALL_SAMPLE
    add constraint fk_sample_patient foreign key (ID_PATIENT)
-      references PATIENT (ID_PATIENT)
+      references ALL_PATIENT (ID_PATIENT)
       on delete restrict on update restrict;
 
