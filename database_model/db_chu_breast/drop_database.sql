79a8250bc6098212075c59aeccf599fe72b87e2f
Ekaterina Flin <ekaterina.flin@univ-grenoble-alpes.fr>
2017-01-31 11:10:19 +0100

    Update db_chu_breast

---
+++ database_model/db_chu_breast/drop_database.sql
@@ -0,0 +1,67 @@
+drop table if exists CHU_ADRESSE cascade;
+
+drop table if exists CHU_ANTECEDENT_GENERAL cascade;
+
+drop table if exists CHU_ANTECEDENT_GYNECO cascade;
+
+drop table if exists CHU_BIOMARQUEUR cascade;
+
+drop table if exists CHU_CODE_TH cascade;
+
+drop table if exists CHU_COMMUNE cascade;
+
+drop table if exists CHU_EVOLUTION cascade;
+
+drop table if exists CHU_GROUPE_MALADIE cascade;
+
+drop table if exists CHU_GROUPE_TOPOGRAPHIE cascade;
+
+drop table if exists CHU_MALADIE cascade;
+
+drop table if exists CHU_METASTASE cascade;
+
+drop table if exists CHU_METHODE_TRAITEMENT cascade;
+
+drop table if exists CHU_MODE_VIE cascade;
+
+drop table if exists CHU_MORPHOLOGIE cascade;
+
+drop table if exists CHU_PATIENT cascade;
+
+drop table if exists CHU_PERFORMANCE_STATUS cascade;
+
+drop table if exists CHU_PHASE_METASTASE cascade;
+
+drop table if exists CHU_PHASE_TUMEUR cascade;
+
+drop table if exists CHU_PRELEVEMENT cascade;
+
+drop table if exists CHU_PRELEVEMENT_BIOMARQUEUR cascade;
+
+drop table if exists CHU_PROFESSION cascade;
+
+drop table if exists CHU_PROTOCOLE_TRAITEMENT cascade;
+
+drop table if exists CHU_REPONSE cascade;
+
+drop table if exists CHU_RESSOURCE_BIOLOGIQUE cascade;
+
+drop table if exists CHU_STATUT_MARITAL cascade;
+
+drop table if exists CHU_SURVIVAL cascade;
+
+drop table if exists CHU_TNM cascade;
+
+drop table if exists CHU_TOPOGRAPHIE cascade;
+
+drop table if exists CHU_TRAITEMENT cascade;
+
+drop table if exists CHU_TUMEUR cascade;
+
+drop table if exists CHU_TYPE_ADRESSE cascade;
+
+drop table if exists CHU_TYPE_PHASE cascade;
+
+drop table if exists CHU_TYPE_PRELEVEMENT cascade;
+
+drop table if exists CHU_TYPE_TRAITEMENT cascade;
